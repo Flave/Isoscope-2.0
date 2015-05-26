8b53b50b80b4dcd45f3bb91157666f906eaf5d20
Flavio Gortana <flaviogortana@gmail.com>
2015-05-26 23:28:50 +0200

    added click eventlistener

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -7,7 +7,15 @@ if(process.env.BROWSER) {
   L = require('leaflet');
 }
 
+
 var App = React.createClass({
+  getDefaultProps: function() {
+    return { 
+      lat: 52,
+      lng: -13,
+      zoom: 12
+    }
+  },
   mixins: [Router.Navigation],
   componentDidMount: function() {
     var mapContainer = this.refs.mapContainer.getDOMNode();
@@ -15,7 +23,8 @@ var App = React.createClass({
       center: [this.props.lat, this.props.lng],
       zoom: this.props.zoom
     })
-      .on('moveend', this.handleMoveMapEnd);
+      .on('moveend', this.handleMoveMapEnd)
+      .on('click', this.handleMapClick);
 
     L.tileLayer('https://{s}.tiles.mapbox.com/v4/flaviogortana.2efcca31/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZmxhdmlvZ29ydGFuYSIsImEiOiJzalRHcS1JIn0.aeJmH09S2p_hjOSs3wuT3w', {
         id: 'examples.map-20v6611k',
@@ -25,6 +34,9 @@ var App = React.createClass({
     this.setState({map: map});
   },
 
+  handleMapClick: function(e) {
+    console.log(e);
+  },
 
   /*
   * Gets the state of the map and adds it to the router state

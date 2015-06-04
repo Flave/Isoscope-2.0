540e87f4b3d44027ea478345eb3547eef7df224c
Flavio Gortana <flaviogortana@gmail.com>
2015-06-04 10:58:12 +0200

    delegate interactivity of map

--- src/components/map/Map.react.js
+++ src/components/map/Map.react.js
@@ -1,56 +1,38 @@
 var React = require('react'),
     Router = require('react-router'),
-    hereApi = require('../../apis/here'),
     L;
 
 if(process.env.BROWSER) {
   L = require('leaflet');
 }
 
+var map;
 
-var App = React.createClass({
+var Map = React.createClass({
   getDefaultProps: function() {
     return { 
       lat: 52,
-      lng: -13,
+      lng: 13,
       zoom: 12
     }
   },
   mixins: [Router.Navigation],
   componentDidMount: function() {
     var mapContainer = this.refs.mapContainer.getDOMNode();
-    var map = L.map(mapContainer, {
+    map = L.map(mapContainer, {
       center: [this.props.lat, this.props.lng],
       zoom: this.props.zoom
     })
-      .on('moveend', this.handleMoveMapEnd)
-      .on('click', this.handleMapClick);
+      .on('moveend', this.props.handleMapMoveEnd)
+      .on('click', this.props.handleClick);
 
     L.tileLayer('https://{s}.tiles.mapbox.com/v4/flaviogortana.2efcca31/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZmxhdmlvZ29ydGFuYSIsImEiOiJzalRHcS1JIn0.aeJmH09S2p_hjOSs3wuT3w', {
         id: 'examples.map-20v6611k',
     }).addTo(map);
-
-    // Is there an alternative to set state which doesn't update the component?
-    this.setState({map: map});
   },
 
-  handleMapClick: function(e) {
-    console.log(e)
-    hereApi.get({startLocation: [12.332, 5.342]});
-  },
-
-  /*
-  * Gets the state of the map and adds it to the router state
-  */
-  handleMoveMapEnd: function(e) {
-    var map = this.state.map;
-    var {lat, lng} = map.getCenter();
-    var zoom = map.getZoom();
-    var mapParams = `${lat},${lng},${zoom}`;
-    
-    // FIX: The position seems to be a bit of when reloading
-    // TBD: Check if there are other params/queries and readd them
-    this.replaceWith('/', {}, {map: mapParams});
+  getMap: function() {
+    return map;
   },
 
   render: function() {
@@ -58,4 +40,4 @@ var App = React.createClass({
   }
 });
 
-module.exports = App;
+module.exports = Map;
